650fbb02dcacf9c4b63238067814d0329416fe35
ironypk <ironypk@gmail.com>
2019-03-26 19:42:58 +1200

    my first page

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.querySelector(".company-contacts__text").onmouseover = function(){
+    let a = document.querySelector(".beep");
+    if(true){
+        a.style = "display:block"
+    } else {
+        a.style = "display:none"
+    }
+}
